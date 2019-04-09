dd9ee5d00265427f786a642ec3cf6a5c36a69210
github username <github email address>
2019-04-09 16:57:49 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -53,5 +53,6 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-  resources :coupons, only: [:index, :show]
+  resources :coupons, only: [:index, :new, :create]
+  get '/coupons/:id', to: 'coupon#show', as: '/coupon'
 end
